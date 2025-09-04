9d3d7d465046d4c49951411b144f8e0deb902597
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-09-04 19:50:57 -0300

    [FIX] - implementing userController

---
+++ src/main/java/br/com/web/services/controllers/UserController.java
@@ -0,0 +1,34 @@
+package br.com.web.services.controllers;
+
+import br.com.web.services.entities.User;
+import br.com.web.services.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
+        Optional<User> user = service.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+}
