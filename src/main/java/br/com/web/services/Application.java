148ba67b2ce1d406215b5325a0aa6b41e87d6582
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-09-04 18:48:39 -0300

    [FEAT] - app booted

---
+++ src/main/java/br/com/web/services/Application.java
@@ -0,0 +1,10 @@
+package br.com.web.services;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
