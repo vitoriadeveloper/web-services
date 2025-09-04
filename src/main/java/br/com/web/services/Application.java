71776bf02a2886997367ae8feb790e8c3603c667
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-09-04 19:26:21 -0300

    [FIX] - add spring application

--- src/main/java/br/com/web/services/Application.java
+++ src/main/java/br/com/web/services/Application.java
@@ -1,10 +1,11 @@
 package br.com.web.services;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        SpringApplication.run(Application.class, args);
     }
 }
