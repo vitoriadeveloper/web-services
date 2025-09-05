33c64733434d61499df180ff79bb9022f6c08520
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-09-05 09:03:37 -0300

    [FEAT] - implementing order service

---
+++ src/main/java/br/com/web/services/services/OrderService.java
@@ -0,0 +1,28 @@
+package br.com.web.services.services;
+
+import br.com.web.services.entities.Order;
+import br.com.web.services.repositories.OrderRepository;
+import br.com.web.services.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Order> findById(Long id) {
+        System.out.println("oi");
+        return repository.findById(id);
+    }
+}
