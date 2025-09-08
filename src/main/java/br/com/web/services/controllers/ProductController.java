2f4585bd3b423bea7d63a1950f0a5e495e2d4c91
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-09-08 20:32:34 -0300

    [FEAT] - Implementing product controller

---
+++ src/main/java/br/com/web/services/controllers/ProductController.java
@@ -0,0 +1,33 @@
+package br.com.web.services.controllers;
+
+import br.com.web.services.entities.Product;
+import br.com.web.services.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable("id") Long id){
+        return  service.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+}
